6d169b759598d811ab194867d8a254acc56fa051
Adhiraj <adhirajsingh0105@gmail.com>
2020-05-31 01:33:23 +0530

    functionsWithMultipleparameters

---
+++ functionsWithMultipleParameters/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int addNumbers(int x,int y){
+    int answer = x + y;
+    return answer;
+
+}
+
+int main()
+{
+    cout << addNumbers(50, 20);
+
+    return 0;
+}
