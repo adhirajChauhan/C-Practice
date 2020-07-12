8654feebd9175855ca8aad7bb817eff849c73eb3
Adhiraj <adhirajsingh0105@gmail.com>
2020-07-12 15:52:35 +0530

    Adding Files

---
+++ CPP/Recursion/SumOfArray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int sumOfArray(int a[], int n){
+
+    if(n == 0){
+        return 0;
+    }
+
+    return a[0] + sumOfArray(a+1, n-1);
+}
+
+int main()
+{
+
+    int a[] = {1,2,3,4,5};
+
+    cout << sumOfArray(a,5);
+
+}
