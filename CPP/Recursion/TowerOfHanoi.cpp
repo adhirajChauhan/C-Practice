66a506a948bc6f4b650b1df113f0e34e6fd579db
Adhiraj <adhirajsingh0105@gmail.com>
2020-07-14 13:57:44 +0530

    Adding Files

--- CPP/Recursion/TowerOfHanoi.cpp
+++ CPP/Recursion/TowerOfHanoi.cpp
@@ -11,10 +11,23 @@ int toh(int n){
     return toh(n-1) + 1 + toh(n-1);
 }
 
+void printSteps(int n, char source, char destination, char helper){
+    if(n == 0){
+        return;
+    }
+
+    printSteps(n-1, source, helper, destination);
+    cout << "Moving disk " << n << " from " << source << " -> " << destination << endl;
+    printSteps(n-1, helper, source, destination);
+
+}
 
 int main()
 {
     int n;
+    cout << "Enter number of steps : " ;
     cin >> n;
-    cout << toh(n);
+    cout << "It will require " << toh(n) << " steps." << endl;
+
+    printSteps(n,'A', 'B', 'C');
 }
